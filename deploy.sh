f83c50d283e3b151e52a5c693b045695d0aceafb
Raman Sharma <raman.sharma@msn.com>
2019-07-28 10:12:05 +0100

    adding travis and updated deploy.sh and Readme.md

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,10 @@
 ## docker image build with latest tage. Replace "rsharma" with your ID
-docker build -t rsharma/pythapp:latest  -f ./pythapp/Dockerfile ./pythapp
+docker build -t rsharma/pythapp:latest -t rsharma/pythapp:$SHA  -f ./pythapp/Dockerfile ./pythapp
 
 ## Push docker images to docker hub, Replace "rsharma" with your ID
 docker push rsharma/pythapp:latest
+docker push rsharma/pythapp:$SHA
 
 
 kubectl apply -f k8s
+kubectl set image deployments/pythapp-deployment server=rsharma/pythapp:$SHA
